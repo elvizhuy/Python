e41d3637af3d2c551e39852c16ebcb1154eb7a39
Nguyễn Ngọc Huy <elvizhuy@gmail.com>
2024-10-02 20:10:32 +0700

    '.'

--- 2.py
+++ 2.py
@@ -1,23 +1,29 @@
 def is_increasing_decreasing(s):
     n = len(s)
+
+    # Phải có đúng 8 ký tự
+    if n != 8:
+        return "NO"
+
+    found_decreasing_point = False
     for i in range(1, n):
-        if s[i] > s[i - 1]:
-            break
+        if s[i] < s[i - 1]:
+            found_decreasing_point = True
+        elif found_decreasing_point and s[i] <= s[i - 1]:
+            return "NO"
+
+    if found_decreasing_point:
+        return "YES"
     else:
-        return False
-    for j in range(i, n):
-        if s[j] <= s[j - 1]:
-            return False
-    return True
+        return "NO"
+
 
 def process_tests():
     t = int(input())
     for _ in range(t):
         s = input().strip()
-        if is_increasing_decreasing(s):
-            print("YES")
-        else:
-            print("NO")
+        print(is_increasing_decreasing(s))
+
 
 # Test case
 process_tests()
