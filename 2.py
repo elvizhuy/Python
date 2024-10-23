6ce7542cd8439ab50a4c2906ad133ae5ae482e88
Nguyễn Ngọc Huy <elvizhuy@gmail.com>
2024-10-23 20:19:25 +0700

    .

--- 2.py
+++ 2.py
@@ -1,11 +1,13 @@
-def digit_product(n):
+def odd_position_digit_product(num):
+    num_str = str(num)
     product = 1
     has_non_zero_digit = False
-    for digit in str(n):
-        if digit != '0':
-            product *= int(digit)
+    for i in range(0, len(num_str), 2):
+        digit = int(num_str[i])
+        if digit != 0:
+            product *= digit
             has_non_zero_digit = True
-    return product if has_non_zero_digit else 0
+    return product if has_non_zero_digit else 1
 
 test_cases = int(input())
 
@@ -13,6 +15,6 @@ for _ in range(test_cases):
     N = int(input())
     A = list(map(int, input().split()))
     
-    A.sort(key=lambda x: (digit_product(x), x))
+    A.sort(key=lambda x: (odd_position_digit_product(x), x))
     
     print(*A)
