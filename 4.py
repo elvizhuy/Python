0fc2d0809baed069435eb393dbba65de6e8b0f2d
Nguyễn Ngọc Huy <elvizhuy@gmail.com>
2024-10-23 20:22:37 +0700

    .

--- 4.py
+++ 4.py
@@ -1,9 +1,10 @@
-# Input handling
 test_cases = int(input())
 
 for _ in range(test_cases):
     N = input().strip()
-    if N[:2] == N[-1:-3:-1]:
+    first_two = N[:2]  
+    last_two = N[-2:]  
+    if first_two == last_two[::-1]:  
         print("YES")
     else:
         print("NO")
